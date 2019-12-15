47070a8422a315f322dc28d327341928cee41ff3
Ilia Sibiryakov <stevefredpatrick@gmail.com>
2019-12-15 20:40:10 +0000

    remove some old lines of code

--- mlvread/mlvread.c
+++ mlvread/mlvread.c
@@ -47,38 +47,10 @@ int main(int argc, char ** argv)
         printf("Allocated = %i, requested = %i\n", allocated_size, return_val);
     } while (return_val != allocated_size);
 
-        // return_size = init_MLVReaderFromFILEs(reader, allocsize, mlv_files, argc-1, 0);
-
-    // size_t mlv_reader_size = sizeof_MLVReaderFromFILEs(mlv_files, argc-1, 0);
-
     for (int f = 1; f < argc; ++f)
     {
         fclose(mlv_files[f-1]);
     }
-
-    // for (int f = 1; f < argc; ++f)
-    // {
-    //     FILE * mlv_file = fopen(argv[f], "r");
-
-    //     puts(argv[f]);
-
-    //     size_t mlv_reader_size = sizeof_MLVReaderFromFILEs(&mlv_file, 1, 0);
-
-    //     fclose(mlv_file);
-    // }
-
-    // for (int f = 1; f < argc; ++f)
-    // {
-    //     puts(argv[f]);
-
-    //     uint64_t mlvsize;
-
-    //     void * mem_mlv = file2mem(argv[f], &mlvsize);
-
-    //     sizeof_MLVReaderFromMemory(&mem_mlv, &mlvsize, 1, 0);
-
-    //     free(mem_mlv);
-    // }
 
     return 0;
 }
