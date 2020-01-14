aea7f7f2f78930e8df081d723106cb783fad5329
Ilia Sibiryakov <stevefredpatrick@gmail.com>
2020-01-14 21:53:18 +0000

    should automatically create libraw on Linux as well now

--- raw2mlv/getlibraw.sh
+++ raw2mlv/getlibraw.sh
@@ -1,34 +1,33 @@
+get_libraw()
+{
+	git clone https://github.com/LibRaw/LibRaw.git -b 0.19-stable;
+	cd LibRaw;
+	make -f makefile.dist;
+	mv lib/libraw_r.a ../libraw_r.a;
+	cd ../;
+}
+
 # Git clone libraw so we have libraw headers
 if [ ! -d "LibRaw" ]; then
-	git clone https://github.com/LibRaw/LibRaw.git -b 0.19-stable
+	echo "Libraw is not present."
+	read -p "Clone and compile libraw source code from Github? [y/n] " yn
+	case $yn in
+		[Yy]* )
+			get_libraw;;
+		[Nn]* ) echo "Ok bye you can't compile raw2mlv then."; exit;;
+		* ) exit;;
+	esac
 fi
 
 if [ ! -e libraw_r.a ]; then
-	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-		echo "libraw_r.a is not present, add libraw_r.a to this folder (version 0.19.* please)"
-		echo "Compile instructions: https://www.libraw.org/docs/Install-LibRaw.html"
-		exit
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		echo "libraw_r.a is not present"
-		read -p "Download libraw_r.a? [y/n] " yn
-		case $yn in
-			[Yy]* )
-				echo "Downloading libraw_r.a ..."
-				wget -O ./libraw.zip https://www.libraw.org/data/LibRaw-0.19.4-MacOSX.zip &> /dev/null
-				unzip libraw.zip &> /dev/null
-				librawfolder=LibRaw-0.19.4
-				cp ./$librawfolder/lib/libraw_r.a ./
-				rm libraw.zip &> /dev/null
-				rm -rf $librawfolder &> /dev/null
-				echo "Downloaded"
-				;;
-			[Nn]* ) echo "Put libraw_r.a in this folder thanks bye."; exit;;
-			* ) exit;;
-		esac
-	else
-		echo "Unknown OS"
-		exit;
-	fi
+	echo "Libraw is not present."
+	read -p "Clone and compile libraw source code from Github? [y/n] " yn
+	case $yn in
+		[Yy]* )
+			get_libraw;;
+		[Nn]* ) echo "Ok bye you can't compile raw2mlv then."; exit;;
+		* ) exit;;
+	esac
 else
 	echo "libraw_r.a exists"
 fi
